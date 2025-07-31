723c50d7922c87d90a25f9d127530aa9056955e8
commedescode <commedescode@gmail.com>
2025-08-01 00:39:21 +0100

    Update MainWindow.h

--- birb/include/MainWindow.h
+++ birb/include/MainWindow.h
@@ -11,35 +11,24 @@ namespace Ui
 }
 QT_END_NAMESPACE
 
-//----------------------------------------------------------------------------------------------------------------------
-/// @file MainWindow.h
-/// @brief Main application window class
-/// @author [Author Name]
-/// @version 1.0
-/// @date [Date]
-//----------------------------------------------------------------------------------------------------------------------
+ 
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
   public:
-    //----------------------------------------------------------------------------------------------------------------------
-    /// @brief Constructor for MainWindow
-    /// @param [in] _parent parent widget (default nullptr)
-    //----------------------------------------------------------------------------------------------------------------------
+     
     MainWindow(QWidget *_parent = nullptr);
 
-    //----------------------------------------------------------------------------------------------------------------------
-    /// @brief Destructor for MainWindow
-    //----------------------------------------------------------------------------------------------------------------------
+     
     ~MainWindow();
 
 private:
-    /// @brief Pointer to the UI elements
+    
     Ui::MainWindow *m_ui;
 
-    /// @brief Pointer to the OpenGL widget for rendering
+   
     NGLScene *m_openGLWidget;
 };
 
